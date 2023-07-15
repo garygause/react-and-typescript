b37b49f363c311f17e98dd3c37296c909841859a
Gary Gause <garygause@gmail.com>
2023-07-15 11:40:02 -0700

    345 completed

--- Section-26-Appendix-Typescript/fetchjson/index.ts
+++ Section-26-Appendix-Typescript/fetchjson/index.ts
@@ -2,6 +2,19 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+  const { id, title, completed } = todo;
+  console.log(`
+    ID: ${id}
+    Title: ${title}
+    Completed: ${completed}
+  `);
 });
