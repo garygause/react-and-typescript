c1b6311288d4341029eb1319af76ef02c9c2e36a
Gary Gause <garygause@gmail.com>
2023-07-15 11:51:29 -0700

    fetchjson complete

--- Section-26-Appendix-Typescript/fetchjson/index.ts
+++ Section-26-Appendix-Typescript/fetchjson/index.ts
@@ -12,9 +12,13 @@ interface Todo {
 axios.get(url).then((response) => {
   const todo = response.data as Todo;
   const { id, title, completed } = todo;
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
     ID: ${id}
     Title: ${title}
     Completed: ${completed}
   `);
-});
+};
